0e2e8b274252ebb622d5cb0a5bce1e5ab4392baf
MikesoWeb <klionpsyx@gmail.com>
2020-01-24 13:49:02 +0000

    work version(no desing)

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-
+from pyperclip import copy
 from tkinter import *
 import random
 
 win = Tk()
-win.title('My_prog_GUI')
+win.title('Генератор паролей 2020')
 win.geometry('500x250+150+150')
 
 
-chars = 'xjvndu57476'
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 
 def generate_pass():
-  
-  #ent_text=textentry.get()
+  in_text = textentry.get()
+ 
+  password = ''
   output.delete(0.0,END)
-  for i in range(12):
+  for i in range(0, int(in_text)):
     password+= random.choice(chars)
+  #copy(password)  -- не работает на веб IDE
   output.insert(0.0, password)
  
-
 
 
 textentry = Entry(win, width = 20, bg = 'white')
 textentry.grid(row=3, column=2)
 
 
-
-output = Text(win, width=30,height=10, wrap = WORD, background='white')
+output = Text(win, width=50, height=10, wrap = WORD, background='white')
 output.grid(row=5, column=0)
 
 Button(win, text='Submit',width=6,command = generate_pass) .grid(row=3,column=0,sticky=W)
