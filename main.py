6987740d86b954e45ec0ba9fdcb6dda804504b1b
MikesoWeb <klionpsyx@gmail.com>
2020-01-24 12:28:01 +0000

    add func gen

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 
 from tkinter import *
+import random
 
 win = Tk()
 win.title('My_prog_GUI')
 win.geometry('500x250+150+150')
 
+
+chars = 'xjvndu57476'
+
+
 def generate_pass():
-  ent_text=textentry.get()
+  
+  #ent_text=textentry.get()
   output.delete(0.0,END)
-  output.insert(0.0, ent_text)
+  for i in range(12):
+    password+= random.choice(chars)
+  output.insert(0.0, password)
  
 
 
 
 textentry = Entry(win, width = 20, bg = 'white')
-textentry.grid(row=3, column=4)
+textentry.grid(row=3, column=2)
 
 
 
-output = Text(win, width=75,height=30, wrap = WORD, background='white')
+output = Text(win, width=30,height=10, wrap = WORD, background='white')
 output.grid(row=5, column=0)
 
 Button(win, text='Submit',width=6,command = generate_pass) .grid(row=3,column=0,sticky=W)
